7e273e13285a5a6c1c8c8f0ac1a17f6b802d562d
Shaza Yusof <wanshafiqahzaharah@gmail.com>
2020-04-29 22:51:30 +0800

    third commit

--- app/src/main/java/com/example/guc_registration_system/Model/CourseModel.java
+++ app/src/main/java/com/example/guc_registration_system/Model/CourseModel.java
@@ -2,57 +2,66 @@ package com.example.guc_registration_system.Model;
 
 public class CourseModel {
 
-    public String courseID,courseName,semester,faculty,programme;
+    public String courseID,courseName,semester,faculty,programme,creditValue;
 
     public CourseModel(){
 
     }
 
-    public CourseModel(String courseID, String courseName, String semester, String faculty, String programme) {
+    public CourseModel(String courseID, String courseName, String semester, String faculty, String programme, String creditValue) {
         this.courseID = courseID;
         this.courseName = courseName;
         this.semester = semester;
         this.faculty = faculty;
         this.programme = programme;
+        this.creditValue = creditValue;
     }
 
     public String getCourseID() {
         return courseID;
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public String getSemester() {
-        return semester;
-    }
-
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public String getProgramme() {
-        return programme;
-    }
-
     public void setCourseID(String courseID) {
         this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
     }
 
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
+    public String getSemester() {
+        return semester;
+    }
+
     public void setSemester(String semester) {
         this.semester = semester;
+    }
+
+    public String getFaculty() {
+        return faculty;
     }
 
     public void setFaculty(String faculty) {
         this.faculty = faculty;
     }
 
+    public String getProgramme() {
+        return programme;
+    }
+
     public void setProgramme(String programme) {
         this.programme = programme;
+    }
+
+    public String getCreditValue() {
+        return creditValue;
+    }
+
+    public void setCreditValue(String creditValue) {
+        this.creditValue = creditValue;
     }
 }
